5276e9b9122dca5008eb8fbe691a0080a6e31a46
alekos <alekosgaitanakis1@gmail.com>
2024-07-05 00:04:32 +0300

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use csv::{WriterBuilder, ReaderBuilder};
+use csv::{WriterBuilder, ReaderBuilder, Reader, StringRecord, Writer};
 use std::{io, process, error::Error, fs::OpenOptions, fs::File};
 
 
@@ -7,11 +7,11 @@ fn main() {
 }
 
 fn read_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(file_path)?;
-
+    let mut reader: Reader<File> = Reader::from_path(file_path)?;
+    
     println!();
     //prints the headers
-    let headers = reader.headers()?;
+    let headers: &StringRecord = reader.headers()?;
     for header in headers.iter() {
         print!("{}, ", header);
     }
@@ -19,7 +19,7 @@ fn read_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
 
     //prints the other rows
     for result in reader.records() {
-        let record = result?;
+        let record: csv::StringRecord = result?;
 
         for field in record.iter() {
             print!("{}, ", field);
@@ -33,9 +33,9 @@ fn read_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
 
 //user writes the data that wants to add and then return the vector new_data of strings 
 fn data_to_insert(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut reader = ReaderBuilder::new().has_headers(true).from_path(file_path)?;
+    let mut reader: Reader<File> = ReaderBuilder::new().has_headers(true).from_path(file_path)?;
 
-    let header_result = reader.headers()?;
+    let header_result: &StringRecord = reader.headers()?;
 
 
     let mut new_data: Vec<String> = Vec::new();
@@ -44,7 +44,7 @@ fn data_to_insert(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     for header in header_result.iter() {
         println!("Write to {}, ", header);
 
-        let mut input_for_header = String::new();
+        let mut input_for_header: String = String::new();
 
         io::stdin().read_line(&mut input_for_header).expect("Failed to read line!");
         let input_for_header: String = input_for_header.trim().to_string();
@@ -58,9 +58,9 @@ fn data_to_insert(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
 
 //adds a new row at the end with the input from data_to_insert function
 fn write_in_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
-    let file = OpenOptions::new().append(true).open(file_path)?;
+    let file: File = OpenOptions::new().append(true).open(file_path)?;
     
-    let mut writer = WriterBuilder::new().has_headers(true).from_writer(file);
+    let mut writer: Writer<File> = WriterBuilder::new().has_headers(true).from_writer(file);
 
 
     let new_data: Vec<String> = data_to_insert(file_path)?;
@@ -74,14 +74,14 @@ fn write_in_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
 //returns the name of the final path of the csv file
 fn input_path_file() -> String{
     //read the name of the csv file from stdin
-    let mut file_path = String::new();
+    let mut file_path: String = String::new();
     println!("Write the name of the csv file: ");
     io::stdin().read_line(&mut file_path).expect("Failed to read line!");
 
     //concat it with the ./ to find the file
-    let file_path = file_path.trim();
-    let path = String::from("./");
-    let final_path =  path + &file_path;
+    let file_path: &str = file_path.trim();
+    let path: String = String::from("./");
+    let final_path: String =  path + &file_path;
 
     final_path
 }
@@ -90,16 +90,16 @@ fn input_path_file() -> String{
 //except the string that the user gives of the specific index of the column and then rewrite the csv file
 //from the content of the vector with the name new_csv_vec
 fn remove_from_csv(file_path: &str, index_for_column: usize, string_to_remove: &str) -> Result<(), Box<dyn Error>> {
-    let file = File::open(file_path)?;
+    let file: File = File::open(file_path)?;
     
-    let mut reader = ReaderBuilder::new().from_reader(file);
+    let mut reader: Reader<File> = ReaderBuilder::new().from_reader(file);
 
-    let mut new_csv_vec = vec![];
+    let mut new_csv_vec: Vec<StringRecord> = vec![];
 
     for result in reader.records() {
-        let record = result?;
+        let record: StringRecord = result?;
 
-        let indx = record.get(index_for_column).ok_or("Invalid index for column!")?;
+        let indx: &str = record.get(index_for_column).ok_or("Invalid index for column!")?;
 
         if indx == string_to_remove {
             continue;
@@ -109,11 +109,11 @@ fn remove_from_csv(file_path: &str, index_for_column: usize, string_to_remove: &
         }
     }
 
-    let header = reader.headers()?;
+    let header: &StringRecord = reader.headers()?;
 
-    let final_csv_file = OpenOptions::new().write(true).truncate(true).open(file_path)?;
+    let final_csv_file: File = OpenOptions::new().write(true).truncate(true).open(file_path)?;
 
-    let mut writer = WriterBuilder::new().from_writer(final_csv_file);
+    let mut writer: Writer<File> = WriterBuilder::new().from_writer(final_csv_file);
 
     writer.write_record(header)?;
 
@@ -131,14 +131,14 @@ fn remove_from_csv(file_path: &str, index_for_column: usize, string_to_remove: &
 fn choice_handler(choice: usize) {
     match choice {
         1 => {
-            let final_path = input_path_file();
+            let final_path: String = input_path_file();
             if let Err(e) = read_csv(&final_path){
                 eprintln!("{}", e);
             }
             start();
         },
         2 => {
-            let final_path = input_path_file();
+            let final_path: String = input_path_file();
             if let Err(e) = write_in_csv(&final_path) {
                 println!("error running example: {e}");
                 process::exit(1);
@@ -146,20 +146,20 @@ fn choice_handler(choice: usize) {
             start();
         },
         3 => {
-            let final_path = input_path_file();
+            let final_path: String = input_path_file();
             println!("Write the index of the column(starts from 0) :");
-            let mut index_for_column = String::new();
+            let mut index_for_column: String = String::new();
 
             io::stdin().read_line(&mut index_for_column).expect("Failed to read line!");
             let index_for_column: usize = index_for_column.trim().parse().expect("Reason");
             
             println!("Write what you want to remove from the column that you wrote before :");
-            let mut string_to_remove = String::new();
+            let mut string_to_remove: String = String::new();
             io::stdin()
                 .read_line(&mut string_to_remove)
                 .expect("Failed to read the line!");
 
-            let string_to_remove = string_to_remove.trim();
+            let string_to_remove: &str = string_to_remove.trim();
 
             if let Err(e) = remove_from_csv(&final_path, index_for_column, &string_to_remove) {
                 println!("error running example: {e}");
@@ -180,7 +180,7 @@ fn choice_handler(choice: usize) {
 
 //asks the user for the input of the choice
 fn start() {
-    let mut choice = String::new();
+    let mut choice: String = String::new();
 
     println!("Choices:");
     println!("    1 for print the csv file,");
